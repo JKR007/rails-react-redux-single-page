bfc09789aee392e4ab8f7f12650a990568bea2fc
jkr <abduqahhor.muhamadjonov@gmail.com>
2019-04-21 16:58:02 +0500

    add react-router, generate component App, set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Forward root to StaticController#index
   root 'static#index'
   namespace :v1, defaults: { format: 'json' } do
     get 'things', to: 'things#index'
+  end
+
+  # Forward all requests to StaticController#index but requests
+  # must be non-Ajax (!req.xhr?) and HTML Mime type (req.format.html?).
+  # This does not include the root ("/") path
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
   end
 end
